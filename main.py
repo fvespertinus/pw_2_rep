5f36a7e576e44ca004beb69642570bafef919bba
Yaroslav <1falco_vespertinus1@mail.ru>
2023-03-17 23:17:46 +0300

    py очередной update

--- main.py
+++ main.py
@@ -11,14 +11,10 @@ app = Flask(__name__)
 def qr():
    msg = request.args.get('msg')
    img = qrcode.make(msg)
-   
+
    buffer = BytesIO()
    img.save(buffer, format="png")
- 
+
    img64 = base64.b64encode(buffer.getvalue())
    return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
-
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0')
 
